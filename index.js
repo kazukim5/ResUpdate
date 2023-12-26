b782018e8a1e41c42689d9429d43b3c1e2cb31fb
Kazuki Matsumoto <kazuki.matsumoto@salesforce.com>
2023-12-26 16:34:17 +0900

    1

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const request = require('request');
 
-const app = express();
-app.use(bodyParser.json());
+// Salesforce認証情報
+const consumerKey = env.consumerKey;
+const consumerSecret = env.consumerSecret;
+const username = env.username;
+const password = env.passoword;
 
-const PORT = process.env.PORT || 3000;
+// Salesforce OAuthエンドポイント
+const tokenRequestUrl = 'https://login.salesforce.com/services/oauth2/token';
 
-// 環境変数からSalesforceのAPIエンドポイントとAPIキーを取得
-const SALESFORCE_ENDPOINT = process.env.SALESFORCE_ENDPOINT;
-const API_KEY = process.env.API_KEY;
-
-app.post('/update-record', (req, res) => {
-  // リクエストボディから更新データを取得
-  const updateData = req.body;
-
-  // SalesforceのApex REST APIにリクエストを送信
+// Salesforceからアクセストークンを取得
+function getSalesforceAccessToken(callback) {
   request.post({
-    url: SALESFORCE_ENDPOINT,
+    url: tokenRequestUrl,
+    form: {
+      grant_type: 'password',
+      client_id: consumerKey,
+      client_secret: consumerSecret,
+      username: username,
+      password: password
+    }
+  }, callback);
+}
+
+// アクセストークンを使用してSalesforceのAPIにレコード更新リクエストを送信
+function updateSalesforceRecord(accessToken, updateData, callback) {
+  const salesforceEndpoint = 'https://あなたのSalesforceインスタンスのURL/services/apexrest/RecordUpdate/';
+
+  request.post({
+    url: salesforceEndpoint,
     headers: {
-      'Authorization': `Bearer ${API_KEY}`,
+      'Authorization': `Bearer ${accessToken}`,
       'Content-Type': 'application/json'
     },
     json: true,
     body: updateData
-  }, (error, response, body) => {
+  }, callback);
+}
+
+// 例: アクセストークンを取得してからレコードを更新
+getSalesforceAccessToken((error, response, body) => {
+  if (error) {
+    console.error('Error getting access token:', error);
+    return;
+  }
+
+  const accessToken = JSON.parse(body).access_token;
+  const updateData = { /* ここに更新データを設定 */ };
+
+  updateSalesforceRecord(accessToken, updateData, (error, response, body) => {
     if (error) {
-      return res.status(500).send('Internal Server Error');
+      console.error('Error updating Salesforce record:', error);
+      return;
     }
-    res.status(200).send(body);
+
+    console.log('Record updated:', body);
   });
 });
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
